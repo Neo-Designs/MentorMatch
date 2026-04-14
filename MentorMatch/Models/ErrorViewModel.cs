033dc770fa7ff2a0c35ffd41004a92a69b9983de
udeshi <udeshisilva20@gmail.com>
2026-04-14 21:19:01 +0530

    Initial commit: project structure creation

---
+++ MentorMatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MentorMatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
